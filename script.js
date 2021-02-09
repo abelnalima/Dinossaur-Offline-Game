456b03e563b75b3384187f9b3f06b0a55fd46855
abelnalima <abel.na.lima@gmail.com>
2021-02-09 09:37:15 -0300

    createCactus function, Collision detection

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let position = 0;
 let isJumping = false;
 
 function handleKeyup(event) {
@@ -10,7 +13,6 @@ function handleKeyup(event) {
 }
 
 function jumpDino() {
-    let position = 0;
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -33,4 +35,30 @@ function jumpDino() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.floor(Math.random() * 6000);
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = <h1 class='gameOver'>Fim de Jogo</h1>;
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyup);
